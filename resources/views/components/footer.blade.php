dd977ada317553ca6d36f4c3a1e7796d0566314c
Be2monkey <D303894@edu.curio.nl>
2024-09-11 15:04:34 +0200

    1

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,1 @@
+<P>dit is een footer</P>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<x-layouts.app>
+    <main>
+        <p>
+            Heb je vragen of wil je met ons in contact komen? Vul het onderstaande formulier in of stuur ons een e-mail op info@smart-solutions.nl. We proberen zo snel mogelijk te reageren!
+        </p>
+    </main>
+</x-layouts.app>
